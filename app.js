efde18daa67de7a19e687d114b9f5fd652d78310
Binodsyangtan <binodsyangtan9761@gmail.com>
2024-08-18 17:56:47 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine','ejs')  //ejs ko kura bujnalai engine le yo code lekhne always
+
+app.get('/',(req,res)=>{
+    const data ={
+        name:"Binod syangtan",
+        age: 21,
+        locaton :"kathmandu"
+    }
+    res.render("home.ejs",{
+        data:data
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render("about.ejs")
+})
+
+
+app.listen(3000,()=>{
+    console.log("project suru varo hai tw nodejs ko ");
+    
+})
